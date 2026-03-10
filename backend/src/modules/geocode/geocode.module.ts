bff2b1cd397ac215de1c5b6f464b344cd9b134f0
Alexsanjr <alex.sandro.1199@gmail.com>
2026-03-10 19:37:49 -0300

    Inicio da implementacao de geocode

---
+++ backend/src/modules/geocode/geocode.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { GeocodeService } from './geocode.service';
+import { GeocodeController } from './geocode.controller';
+
+@Module({
+  providers: [GeocodeService],
+  controllers: [GeocodeController]
+})
+export class GeocodeModule {}
