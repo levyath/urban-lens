cfe5a73221812d7d3c0d3d1a2ec5dea5e609b130
Alexsanjr <alex.sandro.1199@gmail.com>
2026-03-11 07:01:33 -0300

    findNearby e getCategories criados

--- backend/src/modules/places/places.controller.ts
+++ backend/src/modules/places/places.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PlacesService } from './places.service';
 
 @Controller('places')
-export class PlacesController {}
+export class PlacesController {
+  constructor(private readonly placesService: PlacesService) {}
+
+  @Get('near')
+  async findNearby(
+    @Query('lat') lat: number,
+    @Query('lon') lon: number,
+    @Query('radius') radius = 1000,
+    @Query('type') type?: string,
+  ): Promise<unknown[]> {
+    return this.placesService.findNearby(
+      Number(lat),
+      Number(lon),
+      Number(radius),
+      type,
+    );
+  }
+
+  // # TODO: limpar o retorno para aqueles desejados por nosso projeto
+  @Get('categories')
+  async getCategories(): Promise<unknown[]> {
+    return this.placesService.getCategories();
+  }
+}
--- backend/src/modules/places/places.service.ts
+++ backend/src/modules/places/places.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class PlacesService {}
+export class PlacesService {
+  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
+
+  async findNearby(
+    lat: number,
+    lon: number,
+    radius: number,
+    type?: string,
+  ): Promise<unknown[]> {
+    const query = `
+      SELECT
+    name,
+    amenity,
+    ST_Y(ST_Transform(way,4326)) AS lat,
+    ST_X(ST_Transform(way,4326)) AS lon,
+    ST_Distance(
+      ST_Transform(way,4326)::geography,
+      ST_SetSRID(ST_MakePoint($1,$2),4326)::geography
+    ) AS distance
+  FROM planet_osm_point
+  WHERE amenity IS NOT NULL
+  AND name IS NOT NULL
+  AND ($4::text IS NULL OR amenity = $4::text)
+  AND ST_DWithin(
+    ST_Transform(way,4326)::geography,
+    ST_SetSRID(ST_MakePoint($1,$2),4326)::geography,
+    $3
+  )
+  ORDER BY distance
+  LIMIT 20
+  `;
+
+    return await this.dataSource.query(query, [lon, lat, radius, type]);
+  }
+
+  async getCategories(): Promise<unknown[]> {
+    const query = `
+    SELECT DISTINCT amenity
+    FROM planet_osm_point
+    WHERE amenity IS NOT NULL
+    ORDER BY amenity
+  `;
+
+    const result: Array<{ amenity: string }> =
+      await this.dataSource.query(query);
+
+    return result.map((row) => row.amenity);
+  }
+}
